9b99bf299e6ea0ec802f5c4431cb4f48b1fbdc2e
candyslug <david.wip.long@gmail.com>
2022-08-12 17:40:25 +0800

    commented debug

--- scripts/loadNavBar.js
+++ scripts/loadNavBar.js
@@ -7,10 +7,10 @@ var temp = window.location.href.split("/").pop().split(".")[0];
 if (temp == "") {
   temp = "index";
 }
-console.log(temp);
+//console.log(temp);
 
 els = document.getElementsByClassName("nav-link");
 for (var i = 0; i < els.length; i++) {
   const a = els[i].textContent.toLower();
-  console.log(a);
+  //console.log(a);
 }
