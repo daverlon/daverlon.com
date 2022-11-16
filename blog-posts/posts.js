9e5df5e7ae56be57fc2af91de52db6d3abb82992
candyslug <david.wip.long@gmail.com>
2022-11-16 21:23:36 +0800

    fixed posts.js

--- blog-posts/posts.js
+++ blog-posts/posts.js
@@ -4,12 +4,7 @@
 
 
 const all_blog_posts = [
-<<<<<<< HEAD
-  "typing-setup",
-  "next-few-months",
-=======
   "learning-deep-learning-deep",
->>>>>>> 5e3d072 (new post)
   "meet-the-crew",
   "second-post-for-testing",
   "my-first-blog-post"
