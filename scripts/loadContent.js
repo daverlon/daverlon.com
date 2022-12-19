ff1a87425b49245930c26892b05e2f8147a2dfc8
David Long <dav@Davids-MacBook-Air.local>
2022-12-19 15:59:33 +0800

    add active navbar

--- scripts/loadContent.js
+++ scripts/loadContent.js
@@ -7,10 +7,15 @@ if (temp == "") {
 
 
 
+$.get("page-content/" + temp, function (data) {
 
-  $.get("page-content/" + temp, function (data) {
-    const content = document.createElement("div");
-    content.classList.add("main-content-section");
-    content.innerHTML = data;
-    $("#content-placeholder").append(content);
-  });
+  
+
+  pagename = temp.split(".")[0];
+
+  const content = document.createElement("div");
+  content.classList.add("main-content-section");
+  content.innerHTML = data;
+  $("#content-placeholder").append(content);
+});
+
--- scripts/loadNavBar.js
+++ scripts/loadNavBar.js
@@ -1,16 +1,37 @@
 
-$.get("templates/navbar.html", function(data) {
-  $("#navbar-placeholder").replaceWith(data);
-});
+jQuery.ajaxSetup({ async: false });
 
 var temp = window.location.href.split("/").pop().split(".")[0];
 if (temp == "") {
   temp = "index";
 }
+
+
+$.get("templates/navbar.html", function (data) {
+  $("#navbar-placeholder").replaceWith(data);
+});
+
 //console.log(temp);
 
-els = document.getElementsByClassName("nav-link");
-for (var i = 0; i < els.length; i++) {
-  const a = els[i].textContent.toLower();
-  //console.log(a);
-}
+// els = document.getElementsByClassName("nav-link");
+// for (var i = 0; i < els.length; i++) {
+//   const a = els[i].textContent.toLower();
+//   //console.log(a);
+// }
+
+
+
+
+window.addEventListener('load', function () {
+  console.log(temp);
+  var navname = "nav-" + temp.split(".")[0];
+  if (navname == "nav-index") return;
+  console.log("Name: " + navname);
+  var element = document.getElementById(navname);
+  console.log(element);
+  element.classList.add("active");
+});
+
+
+
+jQuery.ajaxSetup({async:true});
