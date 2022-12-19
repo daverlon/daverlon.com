164e151640471de3c93cc79f850cda02b625a932
David Long <dav@Davids-MacBook-Air.local>
2022-12-19 14:58:27 +0800

    fix dynamic position github logo

--- scripts/loadContent.js
+++ scripts/loadContent.js
@@ -4,10 +4,13 @@ if (temp == "") {
   temp = "index.html";
 }
 
-$.get("page-content/" + temp, function(data) {
-  const content = document.createElement("div");
-  content.classList.add("main-content-section");
-  content.innerHTML = data;
-  $("#content-placeholder").append(content);
-});
 
+
+
+
+  $.get("page-content/" + temp, function (data) {
+    const content = document.createElement("div");
+    content.classList.add("main-content-section");
+    content.innerHTML = data;
+    $("#content-placeholder").append(content);
+  });
